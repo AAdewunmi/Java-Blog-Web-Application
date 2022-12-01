107c709437df971600721e65b2909936cab47c94
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-12-01 14:50:31 +0000

    Forty-Four Commit: Implement MyListener Class which handles creating tables in Blog database.

--- src/java/connector/MyListener.java
+++ src/java/connector/MyListener.java
@@ -4,82 +4,56 @@
  */
 package connector;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  *
  * @author adrianadewunmi
  */
-public class MyListener extends HttpServlet {
+import javax.servlet.*;
+import java.sql.*;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MyListener</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MyListener at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+public abstract class MyListener implements ServletContextListener{
+    	
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    public void contextInitialized(ServletContextEvent arg0) {
+        Connection con=null;
+        String user = "abc";
+        String pass = "root";
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/JavaBlogDB", user, pass);
+            PreparedStatement ps2= con.prepareStatement("CREATE SEQUENCE JAVATPOINT MINVALUE 1 MAXVALUE 999999999 INCREMENT BY 1 START WITH 1 NOCACHE NOORDER NOCYCLE");
+		ps2.executeUpdate();
+		PreparedStatement ps=con.prepareStatement("CREATE TABLE  FORUMREG(ID NUMBER,USERNAME VARCHAR2(4000),USERPASS VARCHAR2(4000),EMAIL VARCHAR2(4000),MOBILE NUMBER,ADDRESS VARCHAR2(4000),CONSTRAINT FORUMREG_PK PRIMARY KEY (ID) ENABLE)");
+		ps.executeUpdate();
+		PreparedStatement ps4=con.prepareStatement("CREATE TABLE  FORUMREP(ID NUMBER,COUNTRY VARCHAR2(4000),STATE VARCHAR2(4000),DISTRICT VARCHAR2(4000),POLICE_STATION VARCHAR2(4000),REPORT VARCHAR2(4000),STATUS VARCHAR2(4000),IMAGE BLOB,EMAIL VARCHAR2(4000),POSTEDON DATE,CONSTRAINT FORUMREP_PK PRIMARY KEY (ID) ENABLE)");
+		ps4.executeUpdate();		
+		
+	
+		PreparedStatement  ps5=con.prepareStatement("CREATE TABLE FORUMADVC(RID NUMBER,ID NUMBER,CMT VARCHAR2(4000),EMAIL VARCHAR2(4000),CONSTRAINT FORUMADVC_PK PRIMARY KEY (ID) ENABLE)");
+		ps5.executeUpdate();
+		
+		
+		ps5= con.prepareStatement("CREATE TABLE  FORUMTPC (	ID NUMBER, TOPIC VARCHAR2(4000), EMAIL VARCHAR2(4000),CREATEDON DATE NOT NULL ENABLE,CONSTRAINT FORUMTPC_PK PRIMARY KEY (ID) ENABLE )");
+		ps5.executeUpdate();
+		
+		
+		ps5= con.prepareStatement("CREATE TABLE  FORUMTADVC(ID NUMBER, TID NUMBER, CMT VARCHAR2(4000), EMAIL VARCHAR2(4000),CONSTRAINT FORUMTADVC_PK PRIMARY KEY (ID) ENABLE)");
+		ps5.executeUpdate();
+		Statement stmt=con.createStatement();
+		stmt.executeUpdate("CREATE OR REPLACE TRIGGER  BI_FORUMREG before insert on FORUMREG for each row begin select JAVATPOINT.nextval into :NEW.ID from dual; end");
+		stmt.executeUpdate("CREATE OR REPLACE TRIGGER  BI_FORUMREP before insert on FORUMREP for each row begin select JAVATPOINT.nextval into :NEW.ID from dual;end");
+		stmt.executeUpdate("CREATE OR REPLACE TRIGGER  BI_FORUMADVC before insert on FORUMADVC for each row begin select JAVATPOINT.nextval into :NEW.ID from dual;end");
+		stmt.executeUpdate("CREATE OR REPLACE TRIGGER  BI_FORUMTADVC before insert on FORUMTADVC for each row begin select JAVATPOINT.nextval into :NEW.ID from dual;end");
+		stmt.executeUpdate("CREATE OR REPLACE TRIGGER  BI_FORUMTPC before insert on FORUMTPC for each row begin select JAVATPOINT.nextval into :NEW.ID from dual;end");
+			
+	
+            
+        }catch(Exception e){System.out.println("Exception: "+e);}
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+     public void contextDestroyed(ServletContextEvent arg0) {
+		System.out.println("project undeployed");
+     }
 }
